84b90df887b9f4367a907fc5d90fabfc24dfd1a4
Bocchi7 <128772903+Bocchi7@users.noreply.github.com>
2023-07-19 23:44:15 +0800

    完成基础功能

--- main.py
+++ main.py
@@ -1,87 +1,51 @@
 import argparse
 import json
+import random
+import re
 
 def parser_data():
-    """
-    从命令行读取用户参数
-    做出如下约定：
-    1. -f 为必选参数，表示输入题库文件
-    ...
-
-    :return: 参数
-    """
-    parser = argparse.ArgumentParser(
-        prog="Word filling game",
-        description="A simple game",
-        allow_abbrev=True
-    )
-
+    parser = argparse.ArgumentParser(description="A game.")
     parser.add_argument("-f", "--file", help="题库文件", required=True)
-    # TODO: 添加更多参数
-    
-    args = parser.parse_args()
-    return args
-
-
+    parser.add_argument("-n", "--name", help="文章名字")
+    return parser.parse_args()
 
 def read_articles(filename):
-    """
-    读取题库文件
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.loads(f.read())
 
-    :param filename: 题库文件名
+def replace(article, substitutions):
+    text = article["text"]
+    for key in article["hints"].keys():
+        text = re.sub("\{\{"+key+"\}\}", substitutions[key], text)
+    return text
 
-    :return: 一个字典，题库内容
-    """
-    with open(filename, 'r', encoding="utf-8") as f:
-        # TODO: 用 json 解析文件 f 里面的内容，存储到 data 中
-    
-    return data
-
-
-
-def get_inputs(hints):
-    """
-    获取用户输入
-
-    :param hints: 提示信息
-
-    :return: 用户输入的单词
-    """
-
-    keys = []
-    for hint in hints:
-        print(f"请输入{hint}：")
-        # TODO: 读取一个用户输入并且存储到 keys 当中
-
-    return keys
-
-
-def replace(article, keys):
-    """
-    替换文章内容
-
-    :param article: 文章内容
-    :param keys: 用户输入的单词
-
-    :return: 替换后的文章内容
-
-    """
-    for i in range(len(keys)):
-        # TODO: 将 article 中的 {{i}} 替换为 keys[i]
-        # hint: 你可以用 str.replace() 函数，也可以尝试学习 re 库，用正则表达式替换
-
-    return article
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parser_data()
     data = read_articles(args.file)
     articles = data["articles"]
 
-    # TODO: 根据参数或随机从 articles 中选择一篇文章
-    # TODO: 给出合适的输出，提示用户输入
-    # TODO: 获取用户输入并进行替换
-    # TODO: 给出结果
+    if args.name != None:
+        name = args.name
+    else:
+        name = random.choice(list(articles.keys()))
+    article = articles[name]
+    hints = article["hints"]
+    
+    print(article["text"])
+    print("提示：")
+    for key in hints.keys():
+        hint = hints[key]
+        print("{key} : {hint}".format(key=key,hint=hint))
+    
+    print("请输入你的答案：")
+    answers = {}
+    for key in hints.keys():
+        answers[key]=input("{key} : ".format(key=key))
+    print(answers)
 
+    replaced_text=replace(article, answers)
+    print(replaced_text)
+    
 
-
+    
+    
